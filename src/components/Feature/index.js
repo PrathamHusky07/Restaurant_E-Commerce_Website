a8d8a119483571178af2d1020e53e79e7f368556
PrathamHusky07 <114709279+PrathamHusky07@users.noreply.github.com>
2023-02-11 00:08:15 -0500

    Created an E-Commerce website using React components

---
+++ src/components/Feature/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FeatureContainer, FeatureButton } from './FeatureElements';
+
+const Feature = () => {
+  return (
+    <FeatureContainer>
+      <h1>DEAL OF THE DAY. GRAB ASAP</h1>
+      <p>Coca De Trampo - Spanish sauce topped with sausages, american cheese, tomatoes and olives.</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+};
+
+export default Feature;
---
+++ src/components/Products/data.js
@@ -0,0 +1,110 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import product4 from '../../images/product-4.jpg';
+import product5 from '../../images/product-5.jpg';
+import sweet1 from '../../images/sweet-1.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+import sweet4 from '../../images/sweet-4.jpg';
+import sweet5 from '../../images/sweet-5.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Chicken Pepperoni',
+    desc:
+      'American variety of salami, made from cured chicken meat mixed together and seasoned with chilli pepper.',
+    price: '$25.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Boston Connection',
+    desc:
+      ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, onion, capsicum, jalapenos and pesto.',
+    price: '$20.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Delight',
+    desc:
+      ' Fresh tomatoes, onions, arugula, kale, eggplants, bell peppers, spinach, zucchini, mushrooms and more.',
+    price: '$18.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: product4,
+    alt: 'Pizza',
+    name: 'Paneer Makhani',
+    desc:
+      ' Marinara sauce, basil, paneer cubes, onion, spices, tomatoes, olives, yogurt, and pesto.',
+    price: '20.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: product5,
+    alt: 'Pizza',
+    name: 'Cheese Burst',
+    desc:
+      ' Pizza sauce, basil, grated processed cheese, onion, capsicum and jalapenos.',
+    price: '$22.00',
+    button: 'Add to Cart'
+  }
+  
+
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc:
+      'Belgian chocolate glazed donuts covered in icing with sprinkles on top.',
+    price: '$10.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet3,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc:
+      'Vanilla ice cream covered with hazelnut and belgian chocolate glaze topped with a coco stick.',
+    price: '$15.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet1,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc:
+      'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts.',
+    price: '$10.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet4,
+    alt: 'Brownie',
+    name: 'Chocolate Smoothie',
+    desc:
+      'Delicious smoothie made with cocoa powder,topped with white chocolate pieces, milk of your choice and nuts.',
+    price: '$12.00',
+    button: 'Add to Cart'
+  },
+  {
+    img: sweet5,
+    alt: 'Brownie',
+    name: 'Dry Fruits Falooda',
+    desc:
+      'Mughlai dessert made with rose syrup, vermicelli, sweet basil seeds with milk and served with ice-cream.',
+    price: '$15.00',
+    button: 'Add to Cart'
+  },
+
+
+];
